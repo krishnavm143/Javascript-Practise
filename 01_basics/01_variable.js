91bc920fb4686330cc07f5aa572c2009d7c22214
krishna <krishnavm143@gmail.com>
2022-12-28 01:35:26 +0530

    changes done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId=112345;
+let accountEmail='kitty123@email.com'
+let accountcity="bengaluru"
+let accountPassowrd="12345"
+
+let accountState;
+
+console.table([accountId,accountEmail,accountcity,accountPassowrd,accountState])
