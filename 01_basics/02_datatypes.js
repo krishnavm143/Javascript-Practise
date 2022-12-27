d749c008d6419841d22827afef038ef0209359f2
krishna <krishnavm143@gmail.com>
2022-12-28 01:48:32 +0530

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let isLoggedIn=true;
+let state;
+let nameString="krishna";
+
+// number=2 to power 53
+// bigint
+// string=""
+// boolean=true/false
+// null = standalone value
+// undefined
+// symbol=unique
+
+
+// object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
