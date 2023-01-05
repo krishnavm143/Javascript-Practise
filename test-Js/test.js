a26925afb5f0944041b8d48cd4e6dea89d4c24c8
krishna <krishnavm143@gmail.com>
2023-01-05 14:45:12 +0530

    done changes

--- test-Js/test.js
+++ test-Js/test.js
@@ -1,1 +1,14 @@
-console.log('Hello')
+// console.log('Hello')
+// {
+//     let a=10;
+//     console.log(a)
+// }
+// if(true) true
+var a=100
+{
+    var a=10;
+    let b=20;
+    const c=30;
+    console.log(a)
+}
+console.log(a)
