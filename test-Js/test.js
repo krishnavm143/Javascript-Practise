ccb9fe9918b9c4b9d545676e700b4b0a875e04e3
krishna <krishnavm143@gmail.com>
2023-01-18 16:21:37 +0530

    Update test.js

--- test-Js/test.js
+++ test-Js/test.js
@@ -4,11 +4,22 @@
 //     console.log(a)
 // }
 // if(true) true
-var a=100
-{
-    var a=10;
-    let b=20;
-    const c=30;
-    console.log(a)
+// var a=100
+// {
+//     var a=10;
+//     let b=20;
+//     const c=30;
+//     console.log(a)
+// }
+// console.log(a)
+
+// setimeout function
+
+function x(){
+    var i=10;
+    setTimeout(function(){
+        console.log(i)
+    },3000)
+    console.log('namaste')
 }
-console.log(a)
+x()
